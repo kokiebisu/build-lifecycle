510f28fa5a89e483f80d006138385289dd42541a
Kenichi Okiebisu <a01056715@gmail.com>
2021-02-20 18:19:47 -0800

    chore: stop dist from being pushed

--- dist/bundle.esm.js
+++
@@ -1,7 +0,0 @@
-import { createElement } from 'react';
-
-var RandomComponent = function RandomComponent() {
-  return /*#__PURE__*/createElement("div", null, /*#__PURE__*/createElement("h2", null, "Hi I am a random component"));
-};
-
-export { RandomComponent };
